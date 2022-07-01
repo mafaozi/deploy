e24cb68e95793c15b8e718f4bfe973f9964c337f
mafaozii <mafaozi64@gmail.com>
2022-07-01 09:30:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hells Cuy
         </p>
         <a
           className="App-link"
